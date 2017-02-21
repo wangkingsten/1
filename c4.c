6cbd8e407833e6f3c5a6f8ceb1c81c93480a8735
wangkingsten <wangkingsten@users.noreply.github.com>
2017-02-21 14:59:31 +0800

    Create c4.c

---
+++ c4.c
@@ -0,0 +1,18 @@
+//下列程序的功能为：计算x*y的值并将结果输出。试纠正程序中存在的错误，以实现其功能。运行程序并记下屏幕的输出结果。
+//#include <stdio,h>
+//main()                   
+//{ 
+// int  x, y, a        /*定义整型变量x、y、a */
+//x=2;y=3;
+//A=x*y              /*赋值语句，把x*y的结果赋给变量a */
+//printf ("a=%d\n",a);       /*输出语句*/
+//}
+#include <stdio.h>
+int main()
+{
+	int x,y,a;
+	x=2,y=3;
+	a=x*y;
+	printf("a=%d\n",a);
+	return 0;
+}
